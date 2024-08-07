6420527c91fb7dfd8a80d8de7249b000bd83810c
Convert-Bair <117106188+sanchousser@users.noreply.github.com>
2024-08-07 12:15:14 +0200

    localStorage add

--- app.js
+++ app.js
@@ -15,15 +15,26 @@
 const userEnemy = document.getElementById('user-enemy');
 const userScore = document.getElementById('user-score');
 
-let score = 0;
+
+// Получение текущего счета из localStorage и его преобразование в число
+let score = parseInt(localStorage.getItem('userScore')) || 0;
+if (score >= 20) {
+    userEnemy.src = './img/second-level-dragon.jpg';
+}
+
+// Обновление отображаемого счета
+userScore.textContent = score;
 
 userEnemy.addEventListener('click', onEnemyClick);
 
 function onEnemyClick(e) {
+    // Увеличение счета
     score++;
-    userScore.textContent = score
-    if (score >= 20) {
-        userEnemy.src = './img/second-level-dragon.jpg'
-    }
+    // Сохранение нового значения счета в localStorage
+    localStorage.setItem('userScore', score);
+    // Обновление отображаемого счета
+    userScore.textContent = score;
+    // Проверка условия для смены изображения
 }
+
 
